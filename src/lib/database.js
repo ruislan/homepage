8688aab4d0d46a54639cb8b7957c43573a995b35
Ruislan <z17520@126.com>
2024-01-29 16:50:47 +0800

    update kforum status

--- src/lib/database.js
+++ src/lib/database.js
@@ -117,7 +117,8 @@ const database = {
                 github: 'https://github.com/ruislan/kforum',
                 demo: null,
                 versions: [
-                    { label: 'v0.10.0', state: 'dev' },
+                    { label: 'v0.11.0', state: 'plan' },
+                    { label: 'v0.10.0', state: 'alpha' },
                     { label: 'v0.9.0', state: 'alpha' },
                     { label: 'v0.8.0', state: 'alpha' },
                     { label: 'v0.7.0', state: 'alpha' },
